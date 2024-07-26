d3b97e7ed32f8b5ba7bc0ba08054f06dc9946ffb
Jeffrey Zhang <jzh4ng@berkeley.edu>
2024-07-25 18:45:37 -0700

    add buttons template

--- src/app/Toolbar.js
+++ src/app/Toolbar.js
@@ -1,5 +1,14 @@
 export default function Toolbar() {
+    function handleClick() {
+        alert("You clicked me!");
+    }
+
     return (
-        <div className="toolbar">This is where the toolbar will go</div>
+        <div className="toolbar">
+            This is where the toolbar will go
+            <button onClick={handleClick}>Button 1</button>
+            <button onClick={handleClick}>Button 2</button>
+            <button onClick={handleClick}>Button 3</button>
+        </div>
     );
 }
