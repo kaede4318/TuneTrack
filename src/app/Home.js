085115caea918c39919bdd6022ac25e05202577d
komacat <yeahdsj@gmail.com>
2024-07-25 19:14:37 -0700

    add button to go to pdf viewer

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,13 +1,18 @@
 import './App.css';
+import React from 'react';
 
 const Home = () => {
+  const viewPDF = () => {
+    alert("test!");
+  }
     return (
       <div className="home-container">
         <h1 className="app-name">TuneTrack</h1>
         <div className="upload-container">
-          <label htmlFor="file-upload" className="custom-file-upload">
+          
+          <button onClick={viewPDF} htmlFor="file-upload" className="custom-file-upload">
             Upload Sheet Music
-          </label>
+          </button>
           <input id="file-upload" type="file" accept="application/pdf" />
         </div>
       </div>
