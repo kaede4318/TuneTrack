079f7c304d89944b63badd3e861fa5d4cffe454b
Jeffrey Zhang <jzh4ng@berkeley.edu>
2024-07-25 19:27:19 -0700

    add conditional rendering for annotate/play mode

--- src/app/Toolbar.js
+++ src/app/Toolbar.js
@@ -1,15 +1,47 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 export default function Toolbar() {
+    const [annotateMode, setAnnotateMode] = useState(true);
+    const [isDisabled, setDisabled] = useState(true);
+
     function handleClick() {
         alert("You clicked me!");
     }
 
+    const setMode = () => {
+        console.log("status: "+isDisabled)
+        setAnnotateMode(annotateMode => !annotateMode)
+        setDisabled(isDisabled => !isDisabled)
+
+    }
+
     return (
         <div className="toolbar">
-          <button id="home-button" role="button">
-            Home
-          </button>
+            <div className="static-buttons">
+                <button id="home-button" role="button" onClick={handleClick}>
+                    Home
+                </button>
+                <button id="annotate-mode-button" role="button" disabled={isDisabled} onClick={setMode}>
+                    Annotate Mode
+                </button>
+                <button id="play-mode-button" role="button" disabled={!isDisabled} onClick={setMode}>
+                    Play Mode
+                </button>
+            </div>
+            {annotateMode ? (
+                <div className="annotate-mode-buttons">
+                    <button onClick={handleClick}>
+                        draw
+                    </button>
+                </div>
+            ) : (
+                <div className="play-mode-buttons">
+                    <button onClick={handleClick}>
+                        play
+                    </button>
+                </div>
+            )}
         </div>
     );
   }
